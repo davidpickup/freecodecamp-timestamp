a2f9728073cbf8127f99186a06b4d89f49b44688
David Pickup <pickup_david@hotmail.com>
2016-12-23 15:35:28 +0000

    timestamp microservice works.

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var strftime = require('strftime');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send("Timestamp microservice.");
 })
 
+app.get('/:date', function (req, res) {
+  var num = Number(req.params.date);
+  if (isNaN(num)) {
+    var time = Date.parse(req.params.date);
+  }
+  else{
+    var time = new Date(num*1000);
+  }
+  if (isNaN(time)) {
+    res.send({'unix':null,'natural':null});
+  }
+  else{
+    res.send({'unix':time/1000,'natural':strftime('%B %d, %Y', new Date(time))});
+  }
+});
+
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log('Example app listening on port 8080!');
+});
